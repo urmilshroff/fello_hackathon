c81b1c2f1080aadb505e178d043aa281ad9cb565
urmilshroff <urmilshroff@gmail.com>
2023-12-09 19:27:10 +0530

    feat: no time :(

--- lib/pages/money_page.dart
+++ lib/pages/money_page.dart
@@ -1,6 +1,7 @@
 import 'package:development/providers/selection_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sizer/sizer.dart';
 
 class MoneyPage extends HookConsumerWidget {
   const MoneyPage({super.key});
@@ -11,6 +12,27 @@ class MoneyPage extends HookConsumerWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Saving up for a ${selection?.choice.name} 🎉'),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                  height: 32.h,
+                  child: Image.asset('assets/images/sad.png')),
+              const Text(
+                'No time to complete this :(',
+                style: TextStyle(
+                  fontSize: 18.0,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
